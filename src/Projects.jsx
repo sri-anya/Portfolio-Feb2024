72fe3fb915628ead955b445aeb1ff5e57270f2fa
Aastha Saxena <aasthasaxena100@gmail.com>
2024-01-31 01:52:01 -0500

    layout for projects and blog completed

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -2,8 +2,41 @@ import React from 'react'
 
 const Projects = () => {
   return (
-    <div className='w-full h-[87vh] bg-[#141414] text-white'>
-      My Projects
+    <div className='w-full  bg-[#141414] text-white grid grid-cols-3 gap-10 p-10'>
+      <div className="project flex flex-col items-center justify-center border-[2px] border-[#FBA92C]">
+        <div className="cover"><img className='overflow-auto' src="src\assets\pexels-pixabay-270404.jpg" alt="project image" /></div>
+        <div className="details cursor-pointer bg-[#565656] w-full text-center font-bold py-2">View Code</div>
+      </div>
+      <div className="project flex flex-col items-center justify-center border-[2px] border-[#FBA92C]">
+        <div className="cover"><img className='overflow-auto' src="src\assets\pexels-pixabay-270404.jpg" alt="project image" /></div>
+        <div className="details cursor-pointer bg-[#565656] w-full text-center font-bold py-2">View Code</div>
+      </div>
+      <div className="project flex flex-col items-center justify-center border-[2px] border-[#FBA92C]">
+        <div className="cover"><img className='overflow-auto' src="src\assets\pexels-pixabay-270404.jpg" alt="project image" /></div>
+        <div className="details cursor-pointer bg-[#565656] w-full text-center font-bold py-2">View Code</div>
+      </div>
+      <div className="project flex flex-col items-center justify-center border-[2px] border-[#FBA92C]">
+        <div className="cover"><img className='overflow-auto' src="src\assets\pexels-pixabay-270404.jpg" alt="project image" /></div>
+        <div className="details cursor-pointer bg-[#565656] w-full text-center font-bold py-2">View Code</div>
+      </div>
+      <div className="project flex flex-col items-center justify-center border-[2px] border-[#FBA92C]">
+        <div className="cover"><img className='overflow-auto' src="src\assets\pexels-pixabay-270404.jpg" alt="project image" /></div>
+        <div className="details cursor-pointer bg-[#565656] w-full text-center font-bold py-2">View Code</div>
+      </div>
+      <div className="project flex flex-col items-center justify-center border-[2px] border-[#FBA92C]">
+        <div className="cover"><img className='overflow-auto' src="src\assets\pexels-pixabay-270404.jpg" alt="project image" /></div>
+        <div className="details cursor-pointer bg-[#565656] w-full text-center font-bold py-2">View Code</div>
+      </div>
+      <div className="project flex flex-col items-center justify-center border-[2px] border-[#FBA92C]">
+        <div className="cover"><img className='overflow-auto' src="src\assets\pexels-pixabay-270404.jpg" alt="project image" /></div>
+        <div className="details cursor-pointer bg-[#565656] w-full text-center font-bold py-2">View Code</div>
+      </div>
+     
+
+
+
+      
+      
     </div>
   )
 }
