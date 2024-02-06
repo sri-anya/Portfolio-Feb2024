d1ea190c4bd63e77b70d93071879d32c2fe67c88
Aastha Saxena <aasthasaxena100@gmail.com>
2024-02-06 11:45:23 -0500

    changed data representation

--- src/Blogs.jsx
+++ src/Blogs.jsx
@@ -3,6 +3,15 @@ import NavBar from './NavBar'
 import { useNavigate } from 'react-router-dom'
 
 const Blogs = () => {
+  const blogPosts = [
+    {
+      imageSrc: "/assets/webhostmostblog.png",
+      title: "Host your website for free using webhostmost.com",
+      summary: "Hosting for free might boost your confidence when you are still learning and exploring hosting your websites. Checkout webhostmost.com which helps in doing free hosting.",
+      readMoreLink: "https://medium.com/@aasthasaxena100/how-to-host-your-website-for-free-on-webhostmost-com-b6486d35f0e3",
+    },
+    // Add more blog posts as needed
+  ];
 
   const navigate = useNavigate();
   return (
@@ -12,54 +21,15 @@ const Blogs = () => {
       <div className="cardContainer text-white grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 text-center">
 
 
-        <div className="card flex flex-col justify-center items-center gap-5 border-solid border-[2px]  h-60 px-10 py-4 m-10">
-          <img className='rounded-md' width="101" height="101" src="/assets/webhostmostblog.png" alt="webhostmost blog" />
-          <p className='text-balance'>Host your website for free using webhostmost.com</p>
-          <p className='overflow-clip text-xs h-12'>Hosting for free might boost your confidence when you are still learning and exploring hosting your websites. Checkout webhostmost.com which helps in doing free hosting.</p>
-          <button className='bg-[#565656] px-3  rounded-lg '><a href="https://medium.com/@aasthasaxena100/how-to-host-your-website-for-free-on-webhostmost-com-b6486d35f0e3" target='#'>Read More</a></button>
-        </div>
-        <div className="card flex flex-col justify-center items-center gap-5 border-solid border-[2px]  h-60 px-10 py-4 m-10">
-          <img width="120" height="120" src="/assets/pexels-pixabay-270404.jpg" alt="" />
-          <p>Blog Heading</p>
-          <p className='overflow-clip text-xs h-12'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore nobis assumenda similique quod amet necessitatibus eum ducimus at, temporibus perferendis aut, error dolorem. Sit quos beatae neque consequatur temporibus nobis mollitia, totam exercitationem velit.</p>
-          <button className='bg-[#565656] px-3  rounded-lg '>Read More</button>
-        </div>
-        <div className="card flex flex-col justify-center items-center gap-5 border-solid border-[2px]  h-60 px-10 py-4 m-10">
-          <img width="120" height="120" src="/assets/pexels-pixabay-270404.jpg" alt="" />
-          <p>Blog Heading</p>
-          <p className='overflow-clip text-xs h-12'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore nobis assumenda similique quod amet necessitatibus eum ducimus at, temporibus perferendis aut, error dolorem. Sit quos beatae neque consequatur temporibus nobis mollitia, totam exercitationem velit.</p>
-          <button className='bg-[#565656] px-3  rounded-lg '>Read More</button>
-        </div>
-        <div className="card flex flex-col justify-center items-center gap-5 border-solid border-[2px]  h-60 px-10 py-4 m-10">
-          <img width="120" height="120" src="/assets/pexels-pixabay-270404.jpg" alt="" />
-          <p>Blog Heading</p>
-          <p className='overflow-clip text-xs h-12'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore nobis assumenda similique quod amet necessitatibus eum ducimus at, temporibus perferendis aut, error dolorem. Sit quos beatae neque consequatur temporibus nobis mollitia, totam exercitationem velit.</p>
-          <button className='bg-[#565656] px-3  rounded-lg '>Read More</button>
-        </div>
-        <div className="card flex flex-col justify-center items-center gap-5 border-solid border-[2px]  h-60 px-10 py-4 m-10">
-          <img width="120" height="120" src="/assets/pexels-pixabay-270404.jpg" alt="" />
-          <p>Blog Heading</p>
-          <p className='overflow-clip text-xs h-12'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore nobis assumenda similique quod amet necessitatibus eum ducimus at, temporibus perferendis aut, error dolorem. Sit quos beatae neque consequatur temporibus nobis mollitia, totam exercitationem velit.</p>
-          <button className='bg-[#565656] px-3  rounded-lg '>Read More</button>
-        </div>
-        <div className="card flex flex-col justify-center items-center gap-5 border-solid border-[2px]  h-60 px-10 py-4 m-10">
-          <img width="120" height="120" src="/assets/pexels-pixabay-270404.jpg" alt="" />
-          <p>Blog Heading</p>
-          <p className='overflow-clip text-xs h-12'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore nobis assumenda similique quod amet necessitatibus eum ducimus at, temporibus perferendis aut, error dolorem. Sit quos beatae neque consequatur temporibus nobis mollitia, totam exercitationem velit.</p>
-          <button className='bg-[#565656] px-3  rounded-lg '>Read More</button>
-        </div>
-        <div className="card flex flex-col justify-center items-center gap-5 border-solid border-[2px]  h-60 px-10 py-4 m-10">
-          <img width="120" height="120" src="/assets/pexels-pixabay-270404.jpg" alt="" />
-          <p>Blog Heading</p>
-          <p className='overflow-clip text-xs h-12'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore nobis assumenda similique quod amet necessitatibus eum ducimus at, temporibus perferendis aut, error dolorem. Sit quos beatae neque consequatur temporibus nobis mollitia, totam exercitationem velit.</p>
-          <button className='bg-[#565656] px-3  rounded-lg '>Read More</button>
-        </div>
-        <div className="card flex flex-col justify-center items-center gap-5 border-solid border-[2px]  h-60 px-10 py-4 m-10">
-          <img width="120" height="120" src="/assets/pexels-pixabay-270404.jpg" alt="" />
-          <p>Blog Heading</p>
-          <p className='overflow-clip text-xs h-12'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore nobis assumenda similique quod amet necessitatibus eum ducimus at, temporibus perferendis aut, error dolorem. Sit quos beatae neque consequatur temporibus nobis mollitia, totam exercitationem velit.</p>
-          <button className='bg-[#565656] px-3  rounded-lg '>Read More</button>
-        </div>
+      {blogPosts.map((post, index) => (
+  <div key={index} className="card flex flex-col justify-center items-center gap-5 border-solid border-[2px]  h-60 px-10 py-4 m-10">
+    <img className='rounded-md' width="101" height="101" src={post.imageSrc} alt={post.title} />
+    <p className='text-balance'>{post.title}</p>
+    <p className='overflow-clip text-xs h-12'>{post.summary}</p>
+    <button className='bg-[#565656] px-3 rounded-lg'><a href={post.readMoreLink} target='#'>Read More</a></button>
+  </div>
+))}
+        
 
 
 
